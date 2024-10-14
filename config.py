615f6b0552ebec4167e50ffd3bc88fb9dd8be5d5
Devine-swastik <swastikkesharwani029@gmail.com>
2024-10-14 14:50:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,18 @@
-# Get this value from my.telegram.org/apps
-API_ID = 29230755
-API_HASH = ""
+API_ID = 20481558
+API_HASH = "755306349c91cf9a5e827014ef097842"
 
 # Get this value on Telegram by /id
 OWNER_ID = 6440363814
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = "7289814789:AAHycs8aDrJHS68xC2ZepcCWmKoR_akfVEc"
+BOT_TOKEN = "6116157753:AAE0fGeItvQx0pZpWsjaw_U7IoXe1>
 BOT_NAME = "Ꭰᴇᴠɪɴᴇ Ᏼᴀɴᴀʟʟ"
 BOT_USERNAME = "DevineBanall_bot"
 
-# Optional 
-START_IMG = "https://telegra.ph//file/e45175489f16c43a28e34.jpg"
+# Optional
+START_IMG = "https://telegra.ph//file/e45175489f16c43>
 MUSIC = "https://t.me/devineXmusic_bot"
-SOURCE = "\x68\x74\x74\x70\x73\x3a\x2f\x2f\x67\x69\x74\x68\x75\x62\x2e\x63\x6f\x6d\x2f\x64\x65\x76\x69\x6e\x65\x70\x61\x72\x61\x64\x6f\x78\x2f\x64\x65\x76\x69\x6e\x65\x2d\x62\x61\x6e\x61\x6c\x6c"
+SOURCE = "\x68\x74\x74\x70\x73\x3a\x2f\x2f\x67\x69\x7>
 UPDATE_CHANNEL = "https://t.me/Devine_Network"
+
+
