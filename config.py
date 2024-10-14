b83e05e208b9a1c5dd62bb234aa87a0e2257c308
atomtech-official <atomtechgraphics@gmail.com>
2024-10-14 15:12:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ OWNER_ID = 6440363818
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = "6116157753:AAE0fGeItvQx0pZpWsjaw_U7IoXe1>
 BOT_NAME = "Ꭰᴇᴠɪɴᴇ Ᏼᴀɴᴀʟʟ"
-BOT_USERNAME = "DevineBanall_bot"
+BOT_USERNAME = "DevineBanal.l_bot"
 
 # Optional
 START_IMG = "https://telegra.ph//file/e45175489f16c43>
 MUSIC = "https://t.me/devineXmusic_bot"
 SOURCE = "\x68\x74\x74\x70\x73\x3a\x2f\x2f\x67\x69\x7>
-UPDATE_CHANNEL = "https://t.me/Devine_Network"
+UPDATE_CHANNEL = "https://t.me/Beast_NetworK"
 
 
